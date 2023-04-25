914a363d1a757fb9e3992108186aaadd83b92994
sizzlingkenny <53451293+sizzlingkenny@users.noreply.github.com>
2023-04-25 17:24:46 +0500

    Update datafile.php

--- files/datafile.php
+++ files/datafile.php
@@ -1,1 +1,4 @@
-<?php $datafile = 'https://raw.githubusercontent.com/sizzlingkenny/filmato-movie-script-php/main/data.json';?>
+<?php 
+  $datafile = 'https://raw.githubusercontent.com/sizzlingkenny/filmato-movie-script-php/main/data.json'; //keep this link for auto movie updates
+ // $datafile = 'data.json'; uncomment this line and comment the above line for movies data loaded locally
+?>
